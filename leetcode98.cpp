e156189b59809cac81dfb21a4b911b37c62d1c8d
Yi Sheng Lin <33632760+Howieeee@users.noreply.github.com>
2021-12-02 22:13:39 +0800

    Solution

---
+++ leetcode98.cpp
@@ -0,0 +1,43 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    
+    
+    void in_order(TreeNode* node, vector<int>& arr){
+        if(node == NULL)
+            return;
+        if(node->left)
+            in_order(node->left,arr);
+        arr.push_back(node->val);
+        if(node->right)
+            in_order(node->right,arr);
+    }
+    bool isValidBST(TreeNode* root) {
+        bool vaild =  true;
+        vector<int> arr;
+        in_order(root,arr);
+        
+        set<int> n;
+        for(auto a:arr){
+            if(n.count(a))
+                return false;
+            else
+                n.insert(a);
+        }
+        vector<int> test = arr;
+        sort(test.begin(),test.end());
+        if(arr == test)
+            return true;
+        return false;
+    }
+};
